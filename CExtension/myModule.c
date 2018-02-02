76f05b4f3aed3ca9fc2937307834c423a377548a
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2018-02-02 10:45:04 +0700

    C Extension

---
+++ CExtension/myModule.c
@@ -0,0 +1,37 @@
+#include <Python.h>
+
+int Cfib(int n)
+{
+	if (n < 2)
+		return n;
+	else
+		return Cfib(n-1) + Cfib(n-2);
+}
+
+static PyObject* fib(PyObject* self, PyObject* args)
+{
+	int n;
+
+	if (!PyArg_ParseTuple(args, "i", &n))
+		return NULL;
+
+	return Py_BuildValue("i", Cfib(n));
+}
+
+static PyObject* version(PyObject* self)
+{
+	return Py_BuildValue("s", "Version 1.0");
+}
+
+static PyMethodDef myMethods[] = {
+	{"fib", fib, METH_VARARGS, "Calculates the Fibonacci number."},
+	{"version", (PyCFunction)version, METH_NOARGS, "Return the version."},
+	{NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC initmyModule(void)
+{
+	(void) Py_InitModule("myModule", myMethods);
+}
+
+
